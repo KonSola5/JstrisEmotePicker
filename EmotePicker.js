64774d86e466caef175a2d5d28107f3ae93d00e6
Erick F <erickmack1998@gmail.com>
2020-11-19 22:46:42 -0600

    finished version

--- EmotePicker.js
+++ EmotePicker.js
@@ -1,44 +1,54 @@
-function EmoteSelect(element, emoteIndex, container, openBtn, path) {
-  this.inp = element ? element : document.getElementById("ChatInput");
-  this.emoteIndex = emoteIndex ? emoteIndex : '/index_v3.json'
-  this.container = container
-    ? container
-    : document.getElementById("chatInputArea");
-  this.openBtn = openBtn ? openBtn : document.getElementById("openBtn");
-  this.path = path ? path : "null";
-  this.emoteElem = document.createElement("div");
-  this.emoteElem.classList.add("emotePicker", "container");
-  this.container.appendChild(this.emoteElem);
-  // shameless plug
-  this.comment = document.createComment("Designed and developed by Erickmack");
-  this.emoteElem.appendChild(this.comment);
-
+function EmoteSelect(
+  element,
+  emoteIndex,
+  container,
+  openBtn,
+  path,
+  groupsEmote
+) {
+  this.input = element;
+  this.emoteIndex = emoteIndex;
+  this.container = container;
+  this.openBtn = openBtn;
+  this.path = path;
+  this.groupEmotes = groupsEmote;
   this.init();
 }
 
 EmoteSelect.prototype.init = async function () {
   // init basic layout
+  this.emoteElem = document.createElement("div");
+  this.emoteElem.classList.add("emotePicker");
+  this.container.appendChild(this.emoteElem);
+  // shameless plug
+  this.comment = document.createComment("Designed and developed by Erickmack");
+  this.emoteElem.appendChild(this.comment);
   this.initializeContainers();
   // add EventListener to emote button
   this.openBtn.addEventListener("click", function (e) {
     hideElem(this.emoteElem);
   });
 
-  this.emoteList =  await fetch(this.emoteIndex).then((res) => res.json());
+  this.emoteList =
+    typeof this.emoteIndex === "string"
+      ? await fetch(this.emoteIndex).then((res) => res.json())
+      : this.emoteIndex;
 
   this.initializeEmotes();
+  this.lastUsed();
+  this.hideShowElem();
 };
 
 EmoteSelect.prototype.initializeContainers = function () {
   // create input elem for searching emotes
   this.searchElem = document.createElement("form");
-  this.searchElem.classList.add("form-inline");
+  this.searchElem.classList.add("form-inline", "emoteForm");
   this.emoteElem.appendChild(this.searchElem);
   this.searchBar = document.createElement("input");
   this.searchBar.classList.add("form-control");
   this.searchBar.id = "emoteSearch";
-  this.searchBar.addEventListener("input", ()=>{
-    this.searchFunction(this.emoteList)
+  this.searchBar.addEventListener("input", () => {
+    this.searchFunction(this.emoteList);
   });
   this.searchBar.setAttribute("type", "text");
   this.searchBar.setAttribute("placeholder", "Search Emotes");
@@ -56,28 +66,20 @@ EmoteSelect.prototype.initializeContainers = function () {
 
 EmoteSelect.prototype.initializeEmotes = function () {
   // Emote groups
-  this.groupList = this.emoteList.length > 22 ? [
-    "Jstris",
-    "smileys-emotion",
-    "people-body",
-    "component",
-    "animals-nature",
-    "food-drink",
-    "travel-places",
-    "activities",
-    "objects",
-    "symbols",
-    "flags",
-    "extras-unicode",
-    "extras-openmoji",
-  ] : ['Jstris']
-
+  let groups = [];
+  this.emoteList.forEach((emote) => {
+    let i = groups.findIndex((x) => x === emote["g"]);
+    if (i <= -1) {
+      groups.push(emote["g"]);
+    }
+  });
+  this.groupList = groups;
   this.createGroups(this.groupList);
 };
 
 EmoteSelect.prototype.createGroups = async function (groups) {
   let self = this;
-  this.emojis = this.emoteList
+  this.emojis = this.emoteList;
   // config for intersection observer
   let observerConfig = {
     root: document.querySelector(".emotesWrapper"),
@@ -101,7 +103,8 @@ EmoteSelect.prototype.createGroups = async function (groups) {
     self.groupName.classList.add("groupName");
     self.groupName.innerText = `${group.toUpperCase()}`;
     self.groupDiv.appendChild(self.groupName);
-    self.groupDiv.style.minHeight = `${Math.ceil(grouped.length / 6) * 40}px`;
+    let height = Math.ceil(grouped.length / 6) * 45 + 35.4;
+    self.groupDiv.style.minHeight = `${height}px`;
     self.groupsFragment.appendChild(self.groupDiv);
   });
   this.emotesWrapper.appendChild(this.groupsFragment);
@@ -132,78 +135,77 @@ EmoteSelect.prototype.createImages = async function (emotes, target) {
     threshold: 0,
   };
   // new intersection observer
-   let observer = new IntersectionObserver(onView, observerConfig);
+  let observer = new IntersectionObserver(onView, observerConfig);
 
   // document fragment to append emotes
   this.emotesFragment = document.createDocumentFragment();
   let group = target.getAttribute("data-groupName");
   let grouped = emotes.filter((emote) => emote["g"] === `${group}`);
-    for (let i = 0; i < grouped.length; i++) {
-      self.emoteImg = document.createElement("img");
-      let source = grouped[i]['u'] ? `https://s.jezevec10.com/${grouped[i]['u']}` : `/out/${grouped[i]["n"]}-2.svg`;
-      self.emoteImg.classList.add("emoteImg", "loadingEmote");
-      self.emoteImg.onload = function (e) {
-        e.target.classList.remove("loadingEmote");
-      };
-      observer.observe(self.emoteImg)
-      self.emoteImg.setAttribute("loading", "lazy");
-      self.emoteImg.setAttribute("data-emoteName", `${grouped[i]["n"]}`);
-      self.emoteImg.setAttribute("data-source", source);
-      self.emoteImg.addEventListener("click", (e) => {
-        chatEmote(e.target);
-      });
-      self.emoteImg.addEventListener("mouseover", (e) => {
-        this.showName(e.target);
-      });
-      self.emotesFragment.appendChild(self.emoteImg);
+  for (let i = 0; i < grouped.length; i++) {
+    self.emoteImg = document.createElement("img");
+    let source = grouped[i]["u"]
+      ? `${grouped[i]["u"]}`
+      : `${this.path}${grouped[i]["n"]}.svg`;
+    self.emoteImg.classList.add("emoteImg", "loadingEmote");
+    if (grouped[i]["u"]) {
+      self.emoteImg.classList.add("jstrisEmote");
     }
+    self.emoteImg.onload = function (e) {
+      e.target.classList.remove("loadingEmote");
+    };
+    observer.observe(self.emoteImg);
+    self.emoteImg.setAttribute("loading", "lazy");
+    self.emoteImg.setAttribute("data-emoteName", `${grouped[i]["n"]}`);
+    self.emoteImg.setAttribute("data-source", source);
+    self.emoteImg.addEventListener("click", (e) => {
+      this.chatEmote(e.target);
+      this.setStoredEmotes(e.target);
+    });
+    self.emoteImg.addEventListener("mouseover", (e) => {
+      this.showName(e.target);
+    });
+    self.emotesFragment.appendChild(self.emoteImg);
+  }
 
   target.appendChild(this.emotesFragment);
 
-  function onView(changes,observer){
-    setSource(changes,observer)
+  function onView(changes, observer) {
+    self.setSource(changes, observer);
   }
 };
 
 EmoteSelect.prototype.selectGroup = function () {
-  this.groupEmote = [
-    "https://s.jezevec10.com/res/img/e/jstris.png",
-    "grinning_face",
-    "waving_hand",
-    "red_hair",
-    "monkey_face",
-    "red_apple",
-    "compass",
-    "jack-o-lantern",
-    "crown",
-    "warning",
-    "triangular_flag",
-    "hacker_cat",
-    "narwhal",
-  ];
   this.selectionDiv = document.createElement("div");
-  this.selectionDiv.classList.add("selectionDiv");
-  for (let i = 0; i < this.groupList.length; i++) {
+  this.selectionDiv.id = "selectionDiv";
+  this.groupList.forEach((group) => {
     this.groupImage = document.createElement("img");
     this.groupImage.classList.add("groupLink");
-    this.groupImage.setAttribute("data-groupName", `${this.groupList[i]}`);
+    this.groupImage.setAttribute("data-groupName", `${group}`);
     // scrolling action
     this.groupImage.addEventListener("click", (e) => {
-      let group = e.target.getAttribute("data-groupname");
-      let elem = document.getElementById(group);
-      let topPos = elem.offsetTop - 37;
+      let groupName = e.target.getAttribute("data-groupname");
+      let elem = document.getElementById(groupName);
+      let offsetHeight = this.searchElem.clientHeight;
+      let topPos = elem.offsetTop - offsetHeight;
       this.emotesWrapper.scrollTop = topPos;
     });
     // attributes
-    this.groupImage.setAttribute("title", `${this.groupList[i]}`);
-    if (this.groupList[i] === "Jstris") {
-      this.groupImage.setAttribute("src", `${this.groupEmote[i]}`);
+    this.groupImage.setAttribute("title", `${group}`);
+    this.groupImage.setAttribute("data-toggle", "tooltip");
+    this.groupImage.setAttribute("data-placement", "right");
+    if (group === "Jstris") {
+      this.groupImage.setAttribute("src", `${this.groupEmotes[group]}`);
+      this.groupImage.classList.add("jstrisSelector");
     } else {
-      this.groupImage.setAttribute("src", `/out/${this.groupEmote[i]}-2.svg`);
+      this.groupImage.setAttribute(
+        "src",
+        `${this.path}${this.groupEmotes[group]}.svg`
+      );
     }
     this.selectionDiv.appendChild(this.groupImage);
-  }
+  });
   this.optionsContainer.appendChild(this.selectionDiv);
+  $('[data-toggle="tooltip"]').tooltip();
 };
 
 EmoteSelect.prototype.showName = function (target) {
@@ -216,14 +218,22 @@ EmoteSelect.prototype.searchFunction = function (list) {
   let self = this;
   let options = {
     threshold: 0.3,
-    keys: ["n","t"],
+    keys: [
+      {
+        name: "n",
+        weight: 2,
+      },
+      {
+        name: "t",
+        weight: 1,
+      },
+    ],
   };
   let pattern = document.getElementById("emoteSearch").value;
-
   let searchResults = document.getElementById("searchResults");
 
-  if(!pattern){
-    searchResults.parentNode.removeChild(searchResults)
+  if (!pattern && searchResults != null) {
+    searchResults.parentNode.removeChild(searchResults);
   }
   const fuse = new Fuse(list, options);
   let results = fuse.search(pattern);
@@ -235,8 +245,8 @@ EmoteSelect.prototype.searchFunction = function (list) {
     document
       .getElementsByClassName("emotePicker")[0]
       .appendChild(this.searchResults);
-    searchResults = document.getElementById('searchResults')
-  } else if(searchResults) {
+    searchResults = document.getElementById("searchResults");
+  } else if (searchResults) {
     searchResults.innerHTML = "";
   }
 
@@ -246,54 +256,211 @@ EmoteSelect.prototype.searchFunction = function (list) {
     threshold: 0,
   };
   // new intersection observer
-   let observer = new IntersectionObserver(onView, observerConfig);
+  let observer = new IntersectionObserver(onView, observerConfig);
 
   // create document fragment
   self.resultsFragment = document.createDocumentFragment();
   for (let i = 0; i < results.length; i++) {
-    let result = results[i]['item']
-    let source = result['u'] ? `https://s.jezevec10.com/${result['u']}` : `/out/${result['n']}-2.svg`
+    let result = results[i]["item"];
+    let source = result["u"]
+      ? `${result["u"]}`
+      : `${this.path}${result["n"]}.svg`;
     self.emoteResult = document.createElement("img");
-    self.emoteResult.classList.add("emoteImg", "loadingEmote");
-    self.emoteResult.setAttribute('data-source',source)
+    self.emoteResult.classList.add("emoteImg", "loadingEmote", "resultImg");
+    self.emoteResult.setAttribute("data-source", source);
     self.emoteResult.onload = function (e) {
-          e.target.classList.remove("loadingEmote");
-        };
-    self.emoteResult.setAttribute('title',result['n'])
-    self.emoteResult.setAttribute('data-emoteName',result['n'])
+      e.target.classList.remove("loadingEmote");
+    };
+    self.emoteResult.setAttribute("title", result["n"]);
+    self.emoteResult.setAttribute("data-emoteName", result["n"]);
     self.emoteResult.addEventListener("click", (e) => {
-          chatEmote(e.target);
-        });
-    observer.observe(this.emoteResult)
+      this.chatEmote(e.target);
+      this.setStoredEmotes(e.target);
+    });
+    observer.observe(this.emoteResult);
     self.resultsFragment.appendChild(this.emoteResult);
   }
   // append document fragment to search results div
   searchResults.appendChild(this.resultsFragment);
-
-
-  function onView(changes,observer){
-    setSource(changes,observer)
+  function onView(changes, observer) {
+    self.setSource(changes, observer);
   }
-
 };
 
-function setSource(changes,observer){
-  changes.forEach((change) => {
-        if (change.isIntersecting) {
-          let source = change.target.getAttribute('data-source')
-          change.target.setAttribute('src',source)
-          observer.unobserve(change.target);
-        }
-      });
-}
+EmoteSelect.prototype.setSource = function (changes, observer) {
+  setTimeout(() => {
+    changes.forEach((change) => {
+      if (change.isIntersecting) {
+        let source = change.target.getAttribute("data-source");
+        change.target.setAttribute("src", source);
+        observer.unobserve(change.target);
+      }
+    });
+  }, 400);
+};
 
-function chatEmote (target) {
+EmoteSelect.prototype.chatEmote = function (target) {
   let emoteName = target.getAttribute("data-emoteName");
-  let chat = document.getElementById("chatInput");
-
-  chat.value += `:${emoteName}: `;
+  console.log(this.emoteIndex);
+  this.input.value += `:${emoteName}: `;
 };
 
-setTimeout(() => {
-  let elem = new EmoteSelect();
-}, 1000);
+EmoteSelect.prototype.lastUsed = function () {
+  // localstorage logic
+  if (typeof Storage !== "undefined") {
+    if (!localStorage.lastUsed) {
+      let date = Math.floor(Date.now() / 1000);
+      let emotes = [{ Badger: date }, { jstris: date }];
+      localStorage.setItem("lastUsed", JSON.stringify(emotes));
+    }
+  }
+  // create and add recently used div
+  // get divs
+  let parent = this.emotesWrapper;
+  let jstris = document.getElementById("Jstris");
+  // create recently used div
+  this.recent = document.createElement("div");
+  this.recent.classList.add("emotesGroup");
+  this.groupName = document.createElement("h3");
+  this.groupName.classList.add("groupName");
+  this.lastUsedWrapper = document.createElement("div");
+  this.lastUsedWrapper.id = "usedWrapper";
+  this.groupName.id = "recently-used";
+  this.groupName.innerText = "RECENTLY USED";
+  this.recent.appendChild(this.groupName);
+  this.recent.appendChild(this.lastUsedWrapper);
+  // insert div before other groups
+  parent.insertBefore(this.recent, jstris);
+  // create and add recently used group link
+  //get divs
+  let select = this.selectionDiv;
+  let link = document.getElementsByClassName("groupLink")[0];
+  // create group link
+  this.groupLink = document.createElement("img");
+  this.groupLink.classList.add("groupLink");
+  this.groupLink.setAttribute("data-groupName", "recently-used");
+  this.groupLink.setAttribute("title", "Recently used");
+  this.groupLink.setAttribute("data-toggle", "tooltip");
+  this.groupLink.setAttribute("data-placement", "right");
+  this.groupLink.setAttribute("src", `${this.path}three_oclock.svg`);
+  // add event listener
+  this.groupLink.addEventListener("click", (e) => {
+    let group = e.target.getAttribute("data-groupname");
+    let elem = document.getElementById(group);
+    let topPos = elem.offsetTop - 60;
+    this.emotesWrapper.scrollTop = topPos;
+  });
+  // add group link
+  select.insertBefore(this.groupLink, link);
+  $('[data-toggle="tooltip"]').tooltip();
+};
+
+EmoteSelect.prototype.updateLastUsed = function () {
+  // clear images
+  let emotesContainer = document.getElementById("usedWrapper");
+  emotesContainer.innerHTML = "";
+  // get emote list and emotes in local storage
+  let emoteList = this.emoteList;
+  let used = JSON.parse(localStorage.getItem("lastUsed"));
+  // create fragment to append images
+  let usedFragment = document.createDocumentFragment();
+  // loop through stored emotes
+  let pattern;
+  let source;
+  used.forEach((emote) => {
+    pattern = Object.keys(emote)[0];
+    let result = emoteList.filter((emote) => {
+      return emote["n"] === pattern;
+    })[0];
+    source = result.u ? `${result.u}` : `${this.path}${result["n"]}.svg`;
+    this.usedImage = document.createElement("img");
+    this.usedImage.setAttribute("src", source);
+    this.usedImage.setAttribute("data-emoteName", result["n"]);
+    this.usedImage.classList.add("emoteImg");
+    if (result["u"]) {
+      this.usedImage.classList.add("jstrisEmote");
+    }
+    this.usedImage.addEventListener("click", (e) => {
+      this.chatEmote(e.target);
+      this.setStoredEmotes(e.target);
+    });
+    this.usedImage.addEventListener("mouseover", (e) => {
+      this.showName(e.target);
+    });
+    usedFragment.appendChild(this.usedImage);
+  });
+  emotesContainer.appendChild(usedFragment);
+};
+
+EmoteSelect.prototype.setStoredEmotes = function (target) {
+  const MAX_LENGTH_EMOTES = 30;
+  let emotes = JSON.parse(localStorage.getItem("lastUsed"));
+  let updatedEmotes;
+  let emoteName = target.getAttribute("data-emoteName");
+  if (emotes.length === MAX_LENGTH_EMOTES) {
+    // check if emote exists
+    let exists = false;
+    let i = 0;
+    for (let emote of emotes) {
+      i += 1;
+      if (emoteName in emote) {
+        let name = Object.keys(emote)[0];
+        updatedEmotes = emotes;
+        updatedEmotes[i][name] = Math.floor(Date.now() / 1000);
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      let oldestTime = emotes[0][Object.keys(emotes[0])[0]];
+      let oldestEmote = emotes[0];
+      for (let emote of emotes) {
+        let key = Object.keys(emote)[0];
+        if (emote[key] < oldestTime) {
+          oldestTime = emote[key];
+          oldestEmote = emote;
+        }
+      }
+      updatedEmotes = emotes.filter((emote) => emote !== oldestEmote);
+      let newestEmote = { [emoteName]: Math.floor(Date.now() / 1000) };
+      updatedEmotes.push(newestEmote);
+    }
+  } else if (emotes.length < 30) {
+    // check if emote exists
+    let exists = false;
+    let i = 0;
+    for (let emote of emotes) {
+      if (emoteName in emote) {
+        let name = Object.keys(emote)[0];
+        updatedEmotes = emotes;
+        console.log(updatedEmotes[i]);
+        updatedEmotes[i][name] = Math.floor(Date.now() / 1000);
+        exists = true;
+        break;
+      }
+      i += 1;
+    }
+    if (!exists) {
+      let newestEmote = { [emoteName]: Math.floor(Date.now() / 1000) };
+      console.log(newestEmote);
+      updatedEmotes = emotes;
+      updatedEmotes.push(newestEmote);
+    }
+  }
+  localStorage.setItem("lastUsed", JSON.stringify(updatedEmotes));
+};
+
+EmoteSelect.prototype.hideShowElem = function () {
+  let searchBar = document.getElementById("emoteSearch");
+  this.openBtn.addEventListener("click", () => {
+    searchBar.value = "";
+    let searchDiv = document.getElementById("searchResults");
+    if (searchDiv !== null) {
+      searchDiv.parentNode.removeChild(searchDiv);
+    }
+    this.emotesWrapper.scrollTo(0, 0);
+    this.selectionDiv.scrollTo(0, 0);
+    this.updateLastUsed();
+    this.emoteElem.classList.toggle("open");
+  });
+};
